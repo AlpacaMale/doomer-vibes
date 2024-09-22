95f93bb0844629123ba3ed650d357d6cf115a950
박병준 <alpaca9100@gmail.com>
2024-09-22 13:02:16 +0900

    Correct time logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const time = document.querySelector(".status-bar__column > span:nth-child(2)");
 function getTime() {
   const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
   time.innerText = `${hours}:${minutes}`;
 }
 getTime();
